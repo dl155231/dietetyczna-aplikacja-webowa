a275b932adee688eec5014cdb9682a68d5584268
dl155231 <155231@student.uwm.edu.pl>
2022-01-22 23:44:23 +0100

    heroku

--- project/settings.py
+++ project/settings.py
@@ -138,7 +138,7 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'public/uploads/')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
