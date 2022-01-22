aad4f3c8f44b29095d785f7a1867b7822c719491
dl155231 <155231@student.uwm.edu.pl>
2022-01-22 19:58:20 +0100

    allauth fix

--- project/settings.py
+++ project/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
     'django.contrib.staticfiles',
 
     # 3rd party
@@ -69,6 +70,10 @@ TEMPLATES = [
             ],
         },
     },
+]
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
 ]
 
 WSGI_APPLICATION = 'project.wsgi.application'
@@ -118,7 +123,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
+SITE_ID = 1
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
--- project/urls.py
+++ project/urls.py
@@ -19,7 +19,7 @@ from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('allauth.urls')),
     # path('', TemplateView.as_view(template_name='index.html'))
     path('', include('diet_app.urls')),
-    path('logout/', LogoutView.as_view(), name='logout')
 ]
