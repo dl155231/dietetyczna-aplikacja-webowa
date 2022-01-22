834f03b64a318f8c91fd374b62662a4ec640a5f5
dl155231 <155231@student.uwm.edu.pl>
2022-01-23 00:15:12 +0100

    fix functions

--- diet_app/utils.py
+++ diet_app/utils.py
@@ -3,9 +3,10 @@ from diet_app.models import Nutritionist
 
 def check_user(user):
     is_client = True
-    try:
-        if Nutritionist.objects.get(user=user):
-            is_client = False
-    except Nutritionist.DoesNotExist:
-        is_client = True
+    if user.is_authenticated:
+        try:
+            if Nutritionist.objects.get(user=user):
+                is_client = False
+        except Nutritionist.DoesNotExist:
+            is_client = True
     return is_client
--- diet_app/views.py
+++ diet_app/views.py
@@ -3,6 +3,7 @@
 from datetime import date
 
 # Django
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 
 # Project
@@ -11,24 +12,21 @@ from diet_app.models import DietDay
 from diet_app.utils import check_user
 
 
-class MainView(TemplateView):  # noqa: D101
+class MainView(LoginRequiredMixin, TemplateView):  # noqa: D101
     template_name = 'client_diet.html'
 
     def get_context_data(self, **kwargs):  # noqa: D102
         context = super().get_context_data(**kwargs)
-        if not self.request.user.is_anonymous:
-            if check_user(self.request.user):
+        if check_user(self.request.user):
+            try:
+                diet = Diet.objects.get(user=self.request.user)
+                context['diet_name'] = diet.name_diet
+                context['diet_day'] = (date.today() - diet.day_zero).days
                 try:
-                    diet = Diet.objects.get(user=self.request.user)
-                    context['diet_name'] = diet.name_diet
-                    context['diet_day'] = (date.today()-diet.day_zero).days
-                    try:
-                        context['meals'] = diet.dietday_set.get(day=date.today()).day_meals
-                    except DietDay.DoesNotExist:
-                        context['meals'] = 'Brak'
-                except Diet.DoesNotExist:
-                    return context
-            else:
+                    context['meals'] = diet.dietday_set.get(day=date.today()).day_meals
+                except DietDay.DoesNotExist:
+                    context['meals'] = 'Brak'
+            except Diet.DoesNotExist:
                 return context
         return context
 
@@ -36,10 +34,9 @@ class MainView(TemplateView):  # noqa: D101
 class NotificationListView(TemplateView):
     template_name = 'notification_list.html'
 
-    # def get_context_data(self, **kwargs):  # noqa: D102
-    #     context = super().get_context_data(**kwargs)
-    #     if not self.request.user.is_anonymous:
-    #         if not check_user(self.request.user):
-    #             # context['notifications'] =  tu bedzie lista zgloszen!
-    #             return context
-    #     return context
+    def get_context_data(self, **kwargs):  # noqa: D102
+        context = super().get_context_data(**kwargs)
+        if not check_user(self.request.user):
+            # context['notifications'] =  tu bedzie lista zgloszen!
+            return context
+        return context
