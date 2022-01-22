9158e1532bbf22c188c638f396b815725391577e
dl155231 <155231@student.uwm.edu.pl>
2022-01-22 21:38:02 +0100

    add translation

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,8 +1,10 @@
 """Accounts apps."""
 # Django
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class AccountsConfig(AppConfig):  # noqa: D101
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
+    verbose_name = _('Konta')
