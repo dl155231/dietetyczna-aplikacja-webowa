98a6906657a7ff9ddd1311a688c4971ac70dd9ee
dl155231 <155231@student.uwm.edu.pl>
2022-01-23 15:02:30 +0100

    fix secret key

--- project/settings.py
+++ project/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!n(c8$&bkzt+m@*2*vl^9ho%36-w=$h1avfyfj4nr(_q9uv&-b'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # Application definition
 ALLOWED_HOSTS = ['*']
