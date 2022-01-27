7471df1752f0627ef3f40c991327cc94f8395bba
Mateusz Baran <scgfxpl@onet.pl>
2022-01-27 17:39:03 +0100

    wysoki kontrast

--- static/js/index.js
+++ static/js/index.js
@@ -2,19 +2,46 @@ $(document).ready(()=>{
     if(sessionStorage.getItem('kolor') === 'true'){
         $('body').css('filter', 'hue-rotate(320deg)');
     }
+    if(sessionStorage.getItem('bw-kolor') === 'true'){
+        $('body').css('filter', 'grayscale(100%) contrast(200%)');
+        $('body').css('background-image', '');
+        $('body').css('background-color', 'black');
+        $('a').css('color', 'white');
+    }
     swapColor();
+    blackWhiteColor();
 })
 const swapColor = () =>{
     $('#swap-color').on('click', (e)=>{
         let myStorage = sessionStorage;
         if (myStorage.getItem('kolor') === 'true'){
-            console.log('elo')
             $('body').css('filter', 'hue-rotate(0deg)');
             myStorage.removeItem('kolor');
         }
         else {
             myStorage.setItem('kolor', true);
             $('body').css('filter', 'hue-rotate(320deg)');
+        }
+    })
+}
+
+const  blackWhiteColor = () =>{
+    $('#bw-color').on('click', (e)=>{
+        let myStorage = sessionStorage;
+        if (myStorage.getItem('bw-kolor') === 'true'){
+            $('body').css('filter', '');
+            $('body').css('background-color', '');
+            $('body').css('background-image', 'linear-gradient(to right bottom, #212529, #212529, #212529, #233200)');
+            $('a').removeAttr('style');
+            myStorage.removeItem('bw-kolor');
+        }
+        else {
+            myStorage.setItem('bw-kolor', true);
+            $('body').css('filter', 'grayscale(100%) contrast(200%)');
+            $('body').css('background-image', '');
+            $('body').css('background-color', 'black');
+            $('a').css('color', 'white');
+
         }
     })
 }
